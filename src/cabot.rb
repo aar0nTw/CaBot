535b1aceba601a27fb1bf5e08957376796d50a27
Aaron Huang <aaroms9733@gmail.com>
2016-11-20 02:06:24 +0800

    [ci skip] load plugin

--- src/cabot.rb
+++ src/cabot.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'cabot/cabot'
-require 'handlers/fy'
+# Auto-require
+%w(fy stock).each do |plugin|
+  require File.dirname(__FILE__) + "/handlers/#{plugin}.rb"
+end
 
 def cabot
   @cabot ||= Cabot::Cabot.new ENV["LINE_CHANNEL_SECRET"], ENV["LINE_CHANNEL_TOKEN"]
