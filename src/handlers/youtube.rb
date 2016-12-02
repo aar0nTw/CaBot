d04dd119796f8aaeb4a3d7a72e44dff78a2eaea2
Aaron Huang <aaroms9733@gmail.com>
2016-12-03 01:52:01 +0800

    Update youtube handler

--- src/handlers/youtube.rb
+++ src/handlers/youtube.rb
@@ -3,6 +3,9 @@ require 'json'
 class Youtube
   PATTERN = /^\/youtube\s([\w\W]+)/
   YOUTUB_API_URI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=#{ENV['GOOGLE_API_KEY']}&maxResults=1"
+  VIDEO = 'youtube#video'
+  CHANNEL = 'youtube#channel'
+  PLAYLIST = 'youtube#playlist'
 
   def cmd_name
     "/youtube {key_word}"
@@ -17,19 +20,37 @@ class Youtube
     keyword = text.match(Youtube::PATTERN).captures[0]
     results = get_youtube_results keyword
     video = results['items'][0]
-    thumbnail = video['snippet']['thumbnails']['default']['url']
-    video_path = "https://youtu.be/#{video['id']['videoId']}"
+    if video
+      kind = video['id']['kind']
+      snippet = video['snippet']
+      thumbnail = snippet['thumbnails']['medium']['url']
+      title = snippet['title']
+      description = snippet['description']
+      case kind
+      when VIDEO
+        video_path = "https://youtu.be/#{video['id']['videoId']}"
+      when CHANNEL
+        video_path = "https://youtu.be/#{video['id']['channelId']}"
+      when PLAYLIST
+        video_path = "https://www.youtube.com/playlist?list=#{video['id']['playlistId']}"
+      end
 
-    resp.push({
-      type: :image,
-      originalContentUrl: thumbnail,
-      previewImageUrl: thumbnail
-    })
+      resp.push({
+        type: :image,
+        originalContentUrl: thumbnail,
+        previewImageUrl: thumbnail
+      })
 
-    resp.push({
-      type: :text,
-      text: video_path
-    })
+      resp.push({
+        type: :text,
+        text: "#{title}\n#{description}\n#{video_path}\n"
+      })
+    else
+      resp.push({
+        type: :text,
+        text: '找不到影片。'
+      })
+    end
   end
 
   def get_youtube_results(keyword)
