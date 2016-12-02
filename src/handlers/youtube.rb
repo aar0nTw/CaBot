840d79a0b1ad67d757274b180a37ad10663fea3e
Aaron Huang <aaroms9733@gmail.com>
2016-12-02 16:23:17 +0800

    Add youtube

---
+++ src/handlers/youtube.rb
@@ -0,0 +1,43 @@
+require 'json'
+
+class Youtube
+  PATTERN = /^\/youtube\s([\w\W]+)/
+  YOUTUB_API_URI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=#{ENV['GOOGLE_API_KEY']}&maxResults=1"
+
+  def cmd_name
+    "/youtube {key_word}"
+  end
+
+  def manual
+    "Youtube 影片"
+  end
+
+  def reply(text)
+    resp = []
+    keyword = text.match(Youtube::PATTERN).captures[0]
+    results = get_youtube_results keyword
+    video = results['items'][0]
+    thumbnail = video['snippet']['thumbnails']['default']['url']
+    video_path = "https://youtu.be/#{video['id']['videoId']}"
+
+    resp.push({
+      type: :image,
+      originalContentUrl: thumbnail,
+      previewImageUrl: thumbnail
+    })
+
+    resp.push({
+      type: :text,
+      text: video_path
+    })
+  end
+
+  def get_youtube_results(keyword)
+    uri =  URI("#{Youtube::YOUTUB_API_URI}&q=#{keyword}")
+    response = Net::HTTP.get uri
+    JSON.parse(response)
+  end
+
+end
+
+Cabot::Core::CommandProcessor.register_rule(Stock::PATTERN, Stock)
